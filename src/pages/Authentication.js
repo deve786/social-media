f64cb54c0aa2e17173300b90b498ff5ec6874343
Developer <{ID}+{username}@users.noreply.github.com>
2024-07-01 18:03:10 +0530

    AuthenticationRespo

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -4,7 +4,6 @@ import { loginAPI, registerAPI } from '../services/allAPI';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 function Authentication({ register }) {
     const [inputs, setInputs] = useState({
         username: '',
@@ -48,11 +47,8 @@ function Authentication({ register }) {
 
             const result = await loginAPI(inputs);
             localStorage.setItem('token', result.token);
-            console.log("Asdasd");
             navigate('/');
             toast.success("Login successfully");
-
-            
         } catch (error) {
             console.error("Login failed:", error);
             toast.error("Login failed");
@@ -61,35 +57,35 @@ function Authentication({ register }) {
 
     return (
         <div className='p-3 flex justify-center items-center h-screen'>
-            <div className='bg-white rounded-xl p-3 py-8 px-8'>
+            <div className='bg-white rounded-xl p-3 py-8 px-8 w-full max-w-lg mx-2'>
                 <h2 className='text-xl font-semibold'>{register ? "Create an Account" : "Login Here"}</h2>
                 <div className='mt-3 flex flex-col gap-4'>
                     {register && (
                         <>
                             <div className='flex flex-col gap-1'>
                                 <label>Email</label>
-                                <input type="email" name='email' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-96' />
+                                <input type="email" name='email' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-full' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label>Full Name</label>
-                                <input type="text" name='fullName' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-96' />
+                                <input type="text" name='fullName' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-full' />
                             </div>
                         </>
                     )}
                     <div className='flex flex-col gap-1'>
                         <label>Username</label>
-                        <input type="text" name='username' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-96' />
+                        <input type="text" name='username' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-full' />
                     </div>
                     <div className='flex flex-col gap-1'>
                         <label>Password</label>
-                        <input type="password" name='password' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-96' />
+                        <input type="password" name='password' onChange={handleInput} className='border bg-slate-300 outline-none p-1 py-2 rounded px-2 w-full' />
                     </div>
                     <div className='flex flex-col gap-1 mt-2'>
-                        <button className='bg-primary-color rounded py-2 hover:bg-fuchsia-500' onClick={register ? handleRegister : handleLogin}>
+                        <button className='bg-primary-color text-white rounded py-2 hover:bg-fuchsia-500 w-full'>
                             {register ? "Create an Account" : "Login"}
                         </button>
                     </div>
-                    <div className='flex flex-col gap-1 mt-2'>
+                    <div className='flex flex-col gap-1 mt-2 text-center'>
                         <p>{register ? "Already have an account? " : "Don't have an account? "}
                             <Link to={register ? '/login' : '/register'}>
                                 <span className='text-blue-600 cursor-pointer hover:underline'>
